a840403b675ad7f7390e3baaf9014b25ec108515
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-10-21 10:54:10 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿namespace Labb1
+{
+    internal class Circle
+    {
+        double _Radius;
+
+
+        public Circle(double radius)
+        {
+            _Radius = radius;
+        }
+
+
+        public double ShowRadius()
+        {
+            return _Radius;
+        }
+
+        public double GetArea(double radius)
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Labb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Circle Circle = new Circle(5);
+            Circle Circle1 = new Circle(6);
+
+            Console.WriteLine($"Circle radius: {Circle.ShowRadius}\n" +
+                $"Circle area: {Circle.GetArea}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
